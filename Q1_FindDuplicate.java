7b4f0a60ae03701bacc4bec83626f39bfeee58f8
Esra Babadagi <esrababadagi@gmail.com>
2023-12-20 00:38:32 -0500

    Find Duplicate Answer Added

--- Q1_FindDuplicate.java
+++ Q1_FindDuplicate.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Q1_FindDuplicate {
     public static void main(String[] args) {
 
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = input.nextLine().toLowerCase();
+        System.out.println("Enter a character");
+        char ch = input.next().toLowerCase().charAt(0);
 
+        int count =0;
+
+        int i = 0;
+        while (i<name.length()){
+            if(name.charAt(i)==ch){
+                count++;
+            }
+            i++;
+        }
+        System.out.println("We have "+count+" times "+ch);
     }
 }
