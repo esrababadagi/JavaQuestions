957d6e53f1422c2b6df5cd29f77397574329bad3
Esra Babadagi <esrababadagi@gmail.com>
2023-12-20 14:30:54 -0500

    Numbers are token

--- javaquestions/Q2_SwapNumbers.java
+++ javaquestions/Q2_SwapNumbers.java
@@ -1,8 +1,18 @@
 package javaquestions;
 
+import java.util.Scanner;
+
 public class Q2_SwapNumbers {
 
     public static void main(String[] args) {
+
+    //Type a code to swap two integers.
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter first number");
+        int a = input.nextInt();
+        System.out.println("Please enter second number");
+        int b = input.nextInt();
 
 
     }
