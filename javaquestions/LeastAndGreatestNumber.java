1ebd0bcf630ec6808568e24a133edc50f5dca807
Esra Babadagi <esrababadagi@gmail.com>
2023-12-21 23:49:01 -0500

    The list is added

--- javaquestions/LeastAndGreatestNumber.java
+++ javaquestions/LeastAndGreatestNumber.java
@@ -1,11 +1,21 @@
 package javaquestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeastAndGreatestNumber {
 
     public static void main(String[] args) {
 
         //Find the sum of the least and the greatest price given in a String list
         //Example: List<String> myList = new List<String>{'$12.99', '$8.25', '$23.60', '$54.45'}; ==> 62.70
+
+        List<String> prices2 = new ArrayList<>();
+        prices2.add("$12.99");
+        prices2.add("$8.25");
+        prices2.add("$23.60");
+        prices2.add("$54.45");
+
 
     }
 }
