f4dd9ba4746ec54f278d5df2d5bd8a68fd298203
Esra Babadagi <esrababadagi@gmail.com>
2023-12-21 23:53:42 -0500

    The list is also added

--- javaquestions/CheckUniqueElementInList.java
+++ javaquestions/CheckUniqueElementInList.java
@@ -1,10 +1,21 @@
 package javaquestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CheckUniqueElementInList {
 
 
     public static void main(String[] args) {
 
         //Check if all elements are unique in an integer List by using loops.
+
+        List<Integer> myList22 = new ArrayList<>();
+        myList22.add(10);
+        myList22.add(31);
+        myList22.add(15);
+        myList22.add(7);
+        myList22.add(12);
+        myList22.add(23);
     }
 }
