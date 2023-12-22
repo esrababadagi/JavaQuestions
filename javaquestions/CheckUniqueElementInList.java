1ccbb311cc6de8e736a5da903c3c9a177e074229
Esra Babadagi <esrababadagi@gmail.com>
2023-12-21 23:53:05 -0500

    Unique Element question is added

---
+++ javaquestions/CheckUniqueElementInList.java
@@ -0,0 +1,10 @@
+package javaquestions;
+
+public class CheckUniqueElementInList {
+
+
+    public static void main(String[] args) {
+
+        //Check if all elements are unique in an integer List by using loops.
+    }
+}
