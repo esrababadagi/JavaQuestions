4d64ed6d3c5b5d07c9ad1ec8fd2447f9de70313e
Esra Babadagi <esrababadagi@gmail.com>
2023-12-21 23:46:24 -0500

    Question is added

---
+++ javaquestions/LeastAndGreatestNumber.java
@@ -0,0 +1,11 @@
+package javaquestions;
+
+public class LeastAndGreatestNumber {
+
+    public static void main(String[] args) {
+
+        //Find the sum of the least and the greatest price given in a String list
+        //Example: List<String> myList = new List<String>{'$12.99', '$8.25', '$23.60', '$54.45'}; ==> 62.70
+
+    }
+}
